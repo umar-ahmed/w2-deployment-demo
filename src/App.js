3cffa6c7c4d8504772aa8de01fe503c06d3fe8b8
Grace Hu <gracek.hu@mail.utoronto.ca>
2020-11-28 12:31:25 -0800

    Create demo React app for W2

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './webdev-logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Deployment Workshop</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>U of T Web Dev Club</h3>
+        <div class="socials">
+          <a href="https://www.facebook.com/uoftweb"><i class="fab fa-facebook"></i></a>
+          <a href="https://www.instagram.com/uoftwebdevclub/"><i class="fab fa-instagram"></i></a>
+          <a href="https://github.com/uoftweb"><i class="fab fa-github"></i></a>
+          <a href="https://discord.gg/M8RQVSRctH"><i class="fab fa-discord"></i></a>
+          <a href="mailto:uoftwebdevclub@gmail.com"><i class="far fa-envelope"></i></a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
